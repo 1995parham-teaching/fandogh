57c8601728c6589251c87bbf4918f6c248d6c070
Parham Alvani <parham.alvani@gmail.com>
2021-06-26 10:54:14 +0430

    feat: Add User Collection Migration

--- internal/cmd/migrate/main.go
+++ internal/cmd/migrate/main.go
@@ -1,19 +1,37 @@
 package migrate
 
 import (
+	"context"
+
 	"github.com/1995parham/fandogh/internal/config"
 	"github.com/1995parham/fandogh/internal/db"
+	"github.com/1995parham/fandogh/internal/store/user"
 	"github.com/spf13/cobra"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 )
 
 const enable = 1
 
 func main(cfg config.Config, logger *zap.Logger) {
-	_, err := db.New(cfg.Database)
+	db, err := db.New(cfg.Database)
 	if err != nil {
 		logger.Fatal("database initiation failed", zap.Error(err))
 	}
+
+	idx, err := db.Collection(user.Collection).Indexes().CreateOne(
+		context.Background(),
+		mongo.IndexModel{
+			Keys:    bson.M{"email": enable},
+			Options: options.Index().SetUnique(true),
+		})
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Info("database index", zap.Any("index", idx))
 }
 
 // Register migrate command.
