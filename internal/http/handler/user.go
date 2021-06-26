474347fdf96bef29b50c60b7da1794d0ff33eb5a
Parham Alvani <parham.alvani@gmail.com>
2021-06-26 13:35:32 +0430

    feat: Add Login Handler

--- internal/http/handler/user.go
+++ internal/http/handler/user.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/1995parham/fandogh/internal/http/jwt"
 	"github.com/1995parham/fandogh/internal/http/request"
+	"github.com/1995parham/fandogh/internal/http/response"
 	"github.com/1995parham/fandogh/internal/model"
 	"github.com/1995parham/fandogh/internal/store/user"
 	"github.com/labstack/echo/v4"
@@ -17,6 +19,7 @@ type User struct {
 	Store  user.User
 	Tracer trace.Tracer
 	Logger *zap.Logger
+	JWT    jwt.JWT
 }
 
 // nolint: wrapcheck
@@ -55,6 +58,55 @@ func (h User) Create(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusCreated, u)
+}
+
+// Login checks given credentials and generate jwt token
+// nolint: wrapcheck
+func (h User) Login(c echo.Context) error {
+	ctx, span := h.Tracer.Start(c.Request().Context(), "handler.url.login")
+	defer span.End()
+
+	var rq request.Login
+
+	if err := c.Bind(&rq); err != nil {
+		span.RecordError(err)
+
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err := rq.Validate(); err != nil {
+		span.RecordError(err)
+
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	u, err := h.Store.Get(ctx, rq.Email)
+	if err != nil {
+		span.RecordError(err)
+
+		if errors.Is(err, user.ErrEmailNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("email %s does not exist", rq.Email))
+		}
+
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	if u.Password != rq.Password {
+		return echo.NewHTTPError(http.StatusUnauthorized, "incorrect password")
+	}
+
+	var res response.Login
+
+	res.User = u
+
+	t, err := h.JWT.NewAccessToken(u)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	res.AccessToken = t
+
+	return c.JSON(http.StatusOK, res)
 }
 
 // Register registers the routes of User handler on given group.
