f1b2a4ee26ce1baf9379daa1ee717750f686d6fc
Parham Alvani <parham.alvani@gmail.com>
2021-06-26 15:11:46 +0430

    feat: Update Home Model

--- internal/model/home.go
+++ internal/model/home.go
@@ -1,11 +1,19 @@
 package model
 
+type Bed int
+
+const (
+	Single Bed = 1
+	Double Bed = 2
+)
+
+// Home represents a home to rent. contract types and room types are string to handle them more easier.
 type Home struct {
 	Title           string
 	Location        string
 	Description     string
 	Peoples         int
-	Room            int
+	Room            string
 	Bed             int
 	Rooms           int
 	Bathrooms       int
@@ -13,7 +21,7 @@ type Home struct {
 	Guest           bool
 	Pet             bool
 	BillsIncluded   bool
-	Contract        int
+	Contract        string
 	SecurityDeposit int
 	Photos          map[string][]byte
 	Price           int
