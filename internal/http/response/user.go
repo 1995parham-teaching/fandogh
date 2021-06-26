21a163a7a88d5b8825bb3d3ba40dd4e2892481a4
Parham Alvani <parham.alvani@gmail.com>
2021-06-26 13:52:32 +0430

    fix: Correct Lint Issues

--- internal/http/response/user.go
+++ internal/http/response/user.go
@@ -4,6 +4,6 @@ import "github.com/1995parham/fandogh/internal/model"
 
 // Login contains the information from the login endpoint in case of successful login.
 type Login struct {
-	AccessToken string `json:"access_token"`
+	AccessToken string `json:"accessToken"`
 	model.User
 }
